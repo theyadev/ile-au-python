3d6c74657523cbe2de25091feaa5fb3f67d38dd7
theyadev <gtheya@gmail.com>
2021-09-28 10:57:50 +0200

    Optimizing function

--- functions/collision.py
+++ functions/collision.py
@@ -2,7 +2,4 @@ import settings
 
 def collision(x, y, p):
     pos = settings.M[p["pos"]["y"]+y][p["pos"]["x"]+x]
-    if pos != 1 and pos != 2  and pos!=3:
-        return False
-    else:
-        return True
+    return True if str(pos) in "12" else False
