13dd30f7d7aafba225a55d1035992f940d3c0692
theyadev <gtheya@gmail.com>
2021-10-05 09:41:07 +0200

    save func is now in settings

--- functions/save.py
+++
@@ -1,8 +0,0 @@
-import json
-from settings import *
-
-
-def save():
-    with open('./player.json', 'w') as json_file:
-        json.dump(p.toJson(), json_file, ensure_ascii=False, indent=4)
-    return
