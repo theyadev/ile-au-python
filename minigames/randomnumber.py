c3b01d584435174dcf29fb26b8771ffc0ddb0cf6
Theya <gtheya@gmail.com>
2021-10-13 14:58:44 +0200

    Added colors

--- minigames/randomnumber.py
+++ minigames/randomnumber.py
@@ -1,23 +1,30 @@
 from random import randint,seed
+from settings import clear
+from Colors import TextColors
 import time
+
+
 def startGame(player_name):
     seed(round(time.time() * 1000))
     numbers_to_find = [randint(1,100),randint(1,100),randint(1,100)]
-    print(numbers_to_find)
     number = 0
-    for number_to_find in numbers_to_find:
+    for index, number_to_find in enumerate(numbers_to_find):
+        clear()
         number_of_try = 0
         max_number_of_try = 6
-        print(f"Devinez le nombre entre 1 et 100, vous avez {max_number_of_try} essais:")
+        if index == 0:
+            print(f"Devinez le nombre {TextColors.CYAN}entre 1 et 100{TextColors.RESET}, vous avez {TextColors.RED}{max_number_of_try}{TextColors.RESET} essais:".center(100), end="\n\n")
+        else:
+            print(f"Bravo ! Devinez le {TextColors.CYAN}nombre suivant{TextColors.RESET}:".center(100), end="\n\n")
         while number != number_to_find:
             if number_of_try >= max_number_of_try:
                 return False
             try:
                 number= int(input())
                 if number < number_to_find:
-                    print("Plus haut !")
+                    print(f"{TextColors.BLUE}Plus haut !\n{TextColors.RESET}")
                 elif number > number_to_find:
-                    print("Plus bas !")
+                    print(f"{TextColors.YELLOW}Plus bas !\n{TextColors.RESET}")
                 elif number_to_find != numbers_to_find[-1]:
                     print('\nBravo ! Nombre à deviner suivant.\n\n')
                 number_of_try += 1
