5c0edb293c82a114116f2cb81b8459089be39a91
theyadev <gtheya@gmail.com>
2021-10-14 11:49:35 +0200

    changed food and water consomation

--- Class/Player.py
+++ Class/Player.py
@@ -1,7 +1,9 @@
 from random import random
 from challengesCollision import checkChallengesCollision
+
+
 class Player:
-    def __init__(self, name, x, y, seed, food = 100, stamina = 100, water = 100, inventory = [], map_mattrix = None, random_items = None, facing = "idle"):
+    def __init__(self, name, x, y, seed, food=100, stamina=100, water=100, inventory=[], map_mattrix=None, random_items=None, facing="idle"):
         self.NAME = name
         self.POS_X = x
         self.POS_Y = y
@@ -12,19 +14,20 @@ class Player:
         self.WATER = int(water)
         self.INVENTORY = inventory
         self.METRIC = "kg"
-        self.MAX_WEIGHT = 5
+        self.MAX_WEIGHT = 8
         self.MAP_MATTRIX = map_mattrix
         self.RANDOM_ITEMS = random_items
-    
+
     def getWeight(self):
         weight = 0
-        weight = sum([item.WEIGHT*item.QUANTITY for item in self.INVENTORY if item.QUANTITY > 0])
+        weight = sum(
+            [item.WEIGHT*item.QUANTITY for item in self.INVENTORY if item.QUANTITY > 0])
         return weight
 
-    def getIndexItem(self, x,y):
-        for index,item in enumerate(self.RANDOM_ITEMS):
+    def getIndexItem(self, x, y):
+        for index, item in enumerate(self.RANDOM_ITEMS):
             if item[1] == x and item[2] == y:
-                return index 
+                return index
         return False
 
     def getName(self):
@@ -45,24 +48,28 @@ class Player:
             pos = self.MAP_MATTRIX[self.POS_Y+y][self.POS_X+x]
             if (self.POS_Y+y == -1 or self.POS_X+x == -1):
                 return True
-            
+
             return True if str(pos) in "124" else False
         except:
             return True
 
     def move(self, x, y, challenges):
-        if self.STAMINA <= 0: return False
+        if self.FOOD <= 0 or self.WATER <= 0:
+            return False
+        if self.STAMINA <= 0:
+            return False
+
         if self.collision(x, y) == False:
             self.POS_X += x
             self.POS_Y += y
-            index_item = self.getIndexItem(self.POS_X,self.POS_Y)
+            index_item = self.getIndexItem(self.POS_X, self.POS_Y)
             if index_item and self.getWeight() < self.MAX_WEIGHT:
                 self.INVENTORY[self.RANDOM_ITEMS[index_item][0]].QUANTITY += 1
                 self.RANDOM_ITEMS.pop(index_item)
 
             self.STAMINA -= 2 if self.STAMINA > 0 else 0
-            self.FOOD -= 0.2 if self.FOOD > 0 else 0
-            self.WATER -= 0.4 if self.WATER > 0 else 0
+            self.FOOD -= 0.25 if self.FOOD > 0 else 0
+            self.WATER -= 0.5 if self.WATER > 0 else 0
             challenge = checkChallengesCollision(
                 challenges, self.POS_X, self.POS_Y)
             if challenge:
@@ -73,17 +80,19 @@ class Player:
             return False
 
     def rest(self, hours):
-        if self.FOOD - 1 <= 0: return False
+        consommation = 3
+        if self.FOOD - consommation <= 0:
+            return False
         for i in range(round(hours)):
-            self.FOOD -= 1
+            self.FOOD -= consommation
             if self.STAMINA + 10 > 100:
                 self.STAMINA = 100
             else:
                 self.STAMINA += 10
         return True
 
-    # Exports 
-    
+    # Exports
+
     def inventoryToJson(self):
         return [{
                 "ID": item.ID,
@@ -92,7 +101,7 @@ class Player:
                 "WEIGHT": item.WEIGHT,
                 "QUANTITY": item.QUANTITY,
                 "CHAR": item.CHAR
-            } for item in self.INVENTORY]
+                } for item in self.INVENTORY]
 
     def toJson(self):
         return {
